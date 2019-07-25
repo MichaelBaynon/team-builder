b701640af498f7d0ba1cb2f1cccca58234025e57
MichaelBaynon <baynonmichael@gmail.com>
2019-07-25 17:51:36 -0400

    wip2

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -2,11 +2,24 @@ import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Form from './components/Form'
+import Member from './components/Member'
 
 function App() {
   
-  const [member, setMember] = useState(0);
+  const [members, setMembers] = useState([]);
   
+const submitMember = member => setMembers([...members, member])
+const update = uMember => 
+setMembers([
+  ...members.map(member => {
+    if(member.id === uMember.id) {
+      return uMember
+    }
+    return member
+  })
+])
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -14,10 +27,27 @@ function App() {
          <a href='#'>Home</a>
          <a href='#'>Login</a>
         </nav>
+        <div className='sideNav'>
+          <a className='sideNavItem'>example</a>
+          <a className='sideNavItem'>example</a>
+          <a className='sideNavItem'>example</a>
+          <a className='sideNavItem'>example</a>
+          <a className='sideNavItem'>example</a>
+          <a className='sideNavItem'>example</a>
+          <a className='sideNavItem'>example</a>
+      </div>
        <h1>Team Builder</h1>
-       <Form />
-      </header>
+       </header>
+       <div>
+       {members.map((member, i) => (
+        <Member member={member} key={i} update={update} />
+      ))}
+      <Form add={submitMember} />
+      </div>
     </div>
+    
+      
+    
   );
 }
 
